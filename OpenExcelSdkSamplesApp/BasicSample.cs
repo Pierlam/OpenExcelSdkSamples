9530791ab2732bf6fff3417203003babcccb462d
pierlam-project <pierlam-project@outlook.com>
2026-03-19 21:05:57 +0100

    Ajoutez des fichiers projet.

---
+++ OpenExcelSdkSamplesApp/BasicSample.cs
@@ -0,0 +1,100 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using OpenExcelSdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenExcelSdkSamplesApp;
+
+/// <summary>
+/// Read some  cell values from an excel file, and display the type and value of the cell.
+/// </summary>
+public class BasicSample
+{
+    public static void DisplaySomeValuesAndTypes()
+    {
+        ExcelProcessor proc = new ExcelProcessor();
+
+        // open an excel file
+        string filename = @".\ExcelFiles\BasicSample.xlsx";
+        ExcelFile excelFile = proc.OpenExcelFile(filename);
+
+        // get the first sheet of the excel file
+        ExcelSheet excelSheet = proc.GetFirstSheet(excelFile);
+
+        //==A2: hello - string
+        // get a cell, if the cell doesn't exists, cell is null, it's not an error 
+        ExcelCell excelCell = proc.GetCellAt(excelSheet, "A2");
+
+        // get the type and the value of cell
+        ExcelCellValue  excelCellValue = proc.GetCellValue(excelSheet, excelCell);
+        Console.WriteLine("A2: Value: {0}, cell type: {1}", excelCellValue.StringValue, excelCellValue.CellType.ToString());
+
+        //==A3: 12 - Integer
+        excelCell = proc.GetCellAt(excelSheet, "A3");
+        excelCellValue = proc.GetCellValue(excelSheet, excelCell);
+        Console.WriteLine("A3: Value: {0}, cell type: {1}", excelCellValue.IntegerValue, excelCellValue.CellType.ToString());
+
+        //==A4: 34,45 - Double
+        excelCell = proc.GetCellAt(excelSheet, "A4");
+        excelCellValue = proc.GetCellValue(excelSheet, excelCell);
+        Console.WriteLine("A4: Value: {0}, cell type: {1}", excelCellValue.DoubleValue, excelCellValue.CellType.ToString());
+
+        //==A5: 12/10/2025 - DateOnly
+        excelCell = proc.GetCellAt(excelSheet, "A5");
+        excelCellValue = proc.GetCellValue(excelSheet, excelCell);
+        Console.WriteLine("A5: Value: {0}, cell type: {1}", excelCellValue.DateOnlyValue.ToString(), excelCellValue.CellType.ToString());
+
+        //==A6: 03:45 - TimeOnly
+        excelCell = proc.GetCellAt(excelSheet, "A6");
+        excelCellValue = proc.GetCellValue(excelSheet, excelCell);
+        Console.WriteLine("A6: Value: {0}, cell type: {1}", excelCellValue.TimeOnlyValue.ToString(), excelCellValue.CellType.ToString());
+
+        //==A7: 1 200 €	- currency, euro
+        excelCell = proc.GetCellAt(excelSheet, "A7");
+        excelCellValue = proc.GetCellValue(excelSheet, excelCell);
+        Console.WriteLine("A7: Value: {0}, cell type: {1}", excelCellValue.DoubleValue, excelCellValue.CellType.ToString());
+
+        //==A8:  $3 400,00 - currency,  US Dollar
+        excelCell = proc.GetCellAt(excelSheet, "A8");
+        excelCellValue = proc.GetCellValue(excelSheet, excelCell);
+        Console.WriteLine("A8: Value: {0}, cell type: {1}", excelCellValue.DoubleValue, excelCellValue.CellType.ToString());
+
+        proc.CloseExcelFile(excelFile);
+
+
+        /* Display result in console:
+=> OpenExcelSdkSamplesApp:
+A2: Value: hello, cell type: String
+A3: Value: 12, cell type: Integer
+A4: Value: 34,45, cell type: Double
+A5: Value: 12/10/2025, cell type: DateOnly
+A6: Value: 03:45, cell type: TimeOnly
+A7: Value: 1200, cell type: Double
+A8: Value: 3400, cell type: Double         
+         */
+    }
+
+
+    public static void DisplayValuesAndTypesRowByRow()
+    {
+        ExcelProcessor proc = new ExcelProcessor();
+
+        // open an excel file
+        string filename = @".\ExcelFiles\BasicSample.xlsx";
+        ExcelFile excelFile = proc.OpenExcelFile(filename);
+
+        // get the first sheet of the excel file
+        ExcelSheet excelSheet = proc.GetFirstSheet(excelFile);
+
+        // scan each row
+        for () { }
+
+        //==A2: hello - string
+        // get a cell, if the cell doesn't exists, cell is null, it's not an error 
+        ExcelCell excelCell = proc.GetCellAt(excelSheet, "A2");
+
+    }
+   }
---
+++ OpenExcelSdkSamplesApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenExcelSdkSamplesApp;
+
+Console.WriteLine("=> OpenExcelSdkSamplesApp:");
+
+// sample code to demonstrate how to use OpenExcelSdk to read and write Excel files
+
+// todo: lire des cellules d'un fichier Excel et les afficher dans la console
+
+// todo: modifier des cells d'un fichier Excel et les enregistrer
+
+BasicSample.DisplaySomeValuesAndTypes();
+
+BasicSample.DisplayValuesAndTypesRowByRow();
